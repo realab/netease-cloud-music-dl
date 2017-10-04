7466bb31546f710aa2502f96adeceb8717138b74
realityone <realityone@me.com>
2017-10-04 18:23:33 +0800

    Enhancement: add album disc number

--- ncm/file_util.py
+++ ncm/file_util.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from mutagen.mp3 import MP3, HeaderNotFoundError
-from mutagen.id3 import ID3, APIC, TPE1, TIT2, TALB, TRCK, error
+from mutagen.id3 import ID3, APIC, TPE1, TIT2, TALB, TRCK, TPOS, error
 from PIL import Image
 
 
@@ -44,9 +44,9 @@ def add_metadata_to_song(file_path, cover_path, song):
     # add album cover
     id3.add(
         APIC(
-            encoding=0,         # 3 is for UTF8, but here we use 0 (LATIN1) for 163, orz~~~
+            encoding=0,  # 3 is for UTF8, but here we use 0 (LATIN1) for 163, orz~~~
             mime='image/jpeg',  # image/jpeg or image/png
-            type=3,             # 3 is for the cover(front) image
+            type=3,  # 3 is for the cover(front) image
             data=open(cover_path, 'rb').read()
         )
     )
@@ -76,6 +76,13 @@ def add_metadata_to_song(file_path, cover_path, song):
         TRCK(
             encoding=3,
             text='{}/{}'.format(song['no'], song['album']['size'])
+        )
+    )
+    # add album disc number
+    id3.add(
+        TPOS(
+            encoding=3,
+            text='{}/{}'.format(song['disc'], '')
         )
     )
     id3.save(v2_version=3)
